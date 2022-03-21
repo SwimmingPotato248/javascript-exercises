90911955747ec88481ca528f13d357c7eae960ee
swimmingpotato <ptphong200i@gmail.com>
2022-03-21 21:47:38 +0700

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array = [], ...pos) {
+    for (let i = 0; i < pos.length; i++) {
+        const num = pos[i];
+        const index = array.indexOf(num);
+        if (index < 0) continue;
+        array.splice(index, 1);
+    }
+    return array
 };
 
 // Do not edit below this line
