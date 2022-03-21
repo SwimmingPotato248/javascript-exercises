c88068e26def680cdfc0845506e692d5428dc6fb
swimmingpotato <ptphong200i@gmail.com>
2022-03-21 18:17:51 +0700

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    let i = string.length; 
+    for (;i >= 0; i--) {
+        reversedString += string.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
