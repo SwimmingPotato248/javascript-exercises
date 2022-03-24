0a766112882e85e95163362e0d3e4aee591d2ee2
Swimming Potato <ptphong200i@gmail.com>
2022-03-24 23:24:20 +0700

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    word = word.replace(/\s+/g,"");
+    const wordSplit = Array.from(word);
+    const reversed = [];
+    for (i = wordSplit.length - 1; i >= 0; i--) {
+        reversed.push(wordSplit[i]);
+    }
+    const wordSplitString = wordSplit.toString();
+    const reversedString = reversed.toString();
+    if (wordSplitString.toLowerCase() === reversedString.toLowerCase()) {
+        return true;
+    };
+    return false;
+    
 };
 
 // Do not edit below this line
