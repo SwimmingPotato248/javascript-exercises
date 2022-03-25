f79f7b80ac1f393a231bc2ba33d35e4128304f89
Swimming Potato <ptphong200i@gmail.com>
2022-03-25 09:55:29 +0700

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    let sequence = [1, 1];
+    member = +member;
+    if (member < 0) return "OOPS";
+    for (let i = 2; i < member; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    };
+    return sequence[member - 1];
 };
 
 // Do not edit below this line
