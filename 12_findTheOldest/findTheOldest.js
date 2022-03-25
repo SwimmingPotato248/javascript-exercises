00c79809c6d9941fe052f8a2300c1f48f3065a5f
Swimming Potato <ptphong200i@gmail.com>
2022-03-25 10:12:13 +0700

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        let lastGuy;
+        if (a.yearOfDeath === undefined) {
+            lastGuy = (new Date().getFullYear()) - a.yearOfBirth;
+        } else lastGuy = a.yearOfDeath - a.yearOfBirth;
+        let nextGuy;
+        if (b.yearOfDeath === undefined) {
+            nextGuy = (new Date().getFullYear()) - b.yearOfBirth;
+        } else nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+    return people[0];
 };
 
 // Do not edit below this line
